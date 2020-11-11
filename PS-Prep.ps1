27ee79e88835c26f069adf47c2008182c142cf48
Justin Hawkins <18131926+JustinH2008@users.noreply.github.com>
2020-11-10 20:32:54 -0600

    Add files via upload

---
+++ PS-Prep.ps1
@@ -0,0 +1,9 @@
+﻿Set-ExecutionPolicy Unrestricted
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
+Powershell-Cmdlet -Confirm:$false
+Install-Module PowershellGet -Force
+Install-Module -Name CredentialManager
+Install-Module -Name ExchangeOnlineManagement
+Install-Module -Name AzureAD
+Install-Module -Name MSOnline
